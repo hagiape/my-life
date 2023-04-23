58492cfe1bcebcb186a8ff07169d58850256f7c5
hagiape <g_chrisf@protonmail.com>
2023-04-23 16:33:09 +0800

    formatted file path and deleted pseudocode - ready for push

--- my-life.py
+++ my-life.py
@@ -7,21 +7,15 @@
 # Enter line: Simple is better than complex.
 # Are there more lines y/n? n 
 
-# read file for appending
 my_life = open('mylife.txt', 'a')
-# while loop
-# input new line
 while True:
     new_line = input('Enter line: ')
-# write line
-# prompt for another line
     my_life.write(new_line + '\n')
     prompt = input('Are there more lines? yes/no: ')
-# conditional input
-# if no then break, if input is neither yes/no then break
     if prompt == 'no':
         break
     else:
         if prompt != 'yes':
             print('Only answer "yes" or "no". Please try running the program again')
             break
+my_life.close()
